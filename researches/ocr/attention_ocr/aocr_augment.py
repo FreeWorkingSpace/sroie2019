afaf6cf5b03b02546d4a375507bf79b2fe86a100
loveorchids <loveorchidsdavid@gmail.com.cn>
2019-05-01 15:44:16 +0900

    remove contrast enhancement

--- researches/ocr/attention_ocr/aocr_augment.py
+++ researches/ocr/attention_ocr/aocr_augment.py
@@ -5,8 +5,8 @@ def aug_aocr(args, bg_color=255):
     aug_list.append(augmenters.Affine(
         scale={"x": (0.8, 1.0), "y": (0.8, 1.0)}, rotate=(-3, 3), cval=bg_color, fit_output=True),
     )
-    aug_list.append(augmenters.AllChannelsCLAHE(tile_grid_size_px =(4, 12), per_channel=False))
-    aug_list.append(augmenters.AddToHueAndSaturation(value=(-10, 10), per_channel=False))
+    #aug_list.append(augmenters.AllChannelsCLAHE(tile_grid_size_px =(4, 12), per_channel=False))
+    #aug_list.append(augmenters.AddToHueAndSaturation(value=(-10, 10), per_channel=False))
     aug_list.append(augmenters.Resize(
         size={"height": args.resize_height, "width": "keep-aspect-ratio"})
     )
